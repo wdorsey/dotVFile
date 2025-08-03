616da83b0dc152c50474c138eb1e80a9da60ea94
Weston Dorsey <westoncdorsey@gmail.com>
2025-08-03 13:13:20 -0500

    ui wip

--- vfile-ui/src/app/page.tsx
+++ vfile-ui/src/app/page.tsx
@@ -1,21 +1,9 @@
-import { getDirectories, getFileInfos } from "./api";
-import DirectoryRecord from "./DirectoryRecord";
-import FileRecord from "./FileRecord";
+import FileExplorer from "./FileExplorer";
 
-export default async function Home() {
-  const dirs = await getDirectories("/");
-  const files = await getFileInfos("/");
-
+export default function Home() {
   return (
-    <div>
-      <div className="m-auto flex w-3xl flex-col">
-        {dirs.map((dir) => (
-          <DirectoryRecord directory={dir} key={dir.name} />
-        ))}
-        {files.map((file) => (
-          <FileRecord fileInfo={file} key={file.name} />
-        ))}
-      </div>
+    <div className="h-full w-full">
+      <FileExplorer />
     </div>
   );
 }
